81b4c6bb6b0329ac85c60059b5492a1f6c8be599
KushagraJaiswar02 <kush.8q@gmail.com>
2025-07-26 01:12:27 +0530

    product schema

---
+++ model/product.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  description: {
+    type: String,
+    trim: true
+  },
+
+  type: {
+    type: String,
+    enum: ['fresh', 'packed'],
+    required: true
+  },
+
+  freshCategory: {
+    type: String,
+    enum: ['veggies', 'dairy'],
+    required: function () {
+      return this.type === 'fresh';
+    }
+  },
+
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+
+  unit: {
+    type: String,
+    enum: ['kg', 'litre', 'pack', 'piece'],
+    required: true
+  },
+
+  imageUrl: {
+    type: String,
+    default: '/images/default-product.jpg' // you can update this
+  },
+
+  vendorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
